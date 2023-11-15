914431cc34ce48213f5ad1a6393ee5646da807ff
idrisgit config --global user.email idrees4ade01@gmail.com <idrees4ade01@gmail.com>
2023-11-15 08:33:31 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// // alert("Yo What's up")
+
+// // function greet(){
+// //     alert("Hello Skyline")
+    
+// // }
+// for (let i = 0; i < 50; i++) {
+//     console.log(i);
+//     if (i%3 == 0) {
+//         console.log(fizz);
+//     }
+//     if (i%5 == 0) {
+//         console.log(buzz);
+//     }
+//     if (i%3 == 0 && i%5 == 0) {
+//         console.log(fizzbuzz);
+//     }
+// }
+let participant = {firstname:"John", lastname:"Bellard", ethnicity:"white", age:"35"};
+console.log(participant.ethnicity);
